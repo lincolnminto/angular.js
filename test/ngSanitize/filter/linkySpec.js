9a5e6d8b1671801967a8064bd237d1f9714a16c2
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2023-02-21 15:50:33 +0100

    Fix npm:angular:20150909

--- test/ngSanitize/filter/linkySpec.js
+++ test/ngSanitize/filter/linkySpec.js
@@ -11,12 +11,12 @@ describe('linky', function() {
 
   it('should do basic filter', function() {
     expect(linky("http://ab/ (http://a/) <http://a/> http://1.2/v:~-123. c “http://example.com” ‘http://me.com’")).
-      toEqual('<a href="http://ab/">http://ab/</a> ' +
-              '(<a href="http://a/">http://a/</a>) ' +
-              '&lt;<a href="http://a/">http://a/</a>&gt; ' +
-              '<a href="http://1.2/v:~-123">http://1.2/v:~-123</a>. c ' +
-              '&#8220;<a href="http://example.com">http://example.com</a>&#8221; ' +
-              '&#8216;<a href="http://me.com">http://me.com</a>&#8217;');
+    toEqual('<a href="http://ab/">http://ab/</a> ' +
+      '(<a href="http://a/">http://a/</a>) ' +
+      '&lt;<a href="http://a/">http://a/</a>&gt; ' +
+      '<a href="http://1.2/v:~-123">http://1.2/v:~-123</a>. c ' +
+      '&#8220;<a href="http://example.com">http://example.com</a>&#8221; ' +
+      '&#8216;<a href="http://me.com">http://me.com</a>&#8217;');
     expect(linky(undefined)).not.toBeDefined();
   });
 
@@ -35,13 +35,13 @@ describe('linky', function() {
 
   it('should handle mailto:', function() {
     expect(linky("mailto:me@example.com")).
-                    toEqual('<a href="mailto:me@example.com">me@example.com</a>');
+    toEqual('<a href="mailto:me@example.com">me@example.com</a>');
     expect(linky("me@example.com")).
-                    toEqual('<a href="mailto:me@example.com">me@example.com</a>');
+    toEqual('<a href="mailto:me@example.com">me@example.com</a>');
     expect(linky("send email to me@example.com, but")).
-      toEqual('send email to <a href="mailto:me@example.com">me@example.com</a>, but');
+    toEqual('send email to <a href="mailto:me@example.com">me@example.com</a>, but');
     expect(linky("my email is \"me@example.com\"")).
-      toEqual('my email is &#34;<a href="mailto:me@example.com">me@example.com</a>&#34;');
+    toEqual('my email is &#34;<a href="mailto:me@example.com">me@example.com</a>&#34;');
   });
 
   it('should handle quotes in the email', function() {
@@ -50,8 +50,10 @@ describe('linky', function() {
 
   it('should handle target:', function() {
     expect(linky("http://example.com", "_blank")).
-      toEqual('<a target="_blank" href="http://example.com">http://example.com</a>');
+    toBeOneOf('<a target="_blank" href="http://example.com">http://example.com</a>',
+      '<a href="http://example.com" target="_blank">http://example.com</a>');
     expect(linky("http://example.com", "someNamedIFrame")).
-      toEqual('<a target="someNamedIFrame" href="http://example.com">http://example.com</a>');
+    toBeOneOf('<a target="someNamedIFrame" href="http://example.com">http://example.com</a>',
+      '<a href="http://example.com" target="someNamedIFrame">http://example.com</a>');
   });
 });
