f68ad21c88b53540cbf732c23daebb9a1ba20a53
Dumitru Corini <dumitru.corini@gmail.com>
2023-03-07 15:07:20 +0100

    chore(release): add angular-sanitize to releases (#18)
    covers [DEV-179](https://bonitasoft.atlassian.net/browse/DEV-179)

--- scripts/github-actions/publish.sh
+++ scripts/github-actions/publish.sh
@@ -14,13 +14,17 @@ ARG_DEFS=(
 )
 
 function init {
-  BOWER_ANGULAR_DIR=$(resolveDir ../../bower-angular)
+  TMP_DIR=$(resolveDir ../../)
   BUILD_DIR=$(resolveDir ../../build)
   NEW_VERSION=$VERSION_NUMBER
   if [ -z $NEW_VERSION ]
    then
     NEW_VERSION=$(cat $BUILD_DIR/version.txt)
   fi
+  REPOS=(
+    angular
+    angular-sanitize
+  )
   # get the npm dist-tag from a custom property (distTag) in package.json
 #  DIST_TAG=$(readJsonProp "package.json" "distTag")
 }
@@ -30,63 +34,74 @@ function prepare {
   #
   # move the files from the build
   #
-  if [ -f $BUILD_DIR/angular.js ] # ignore i18l
-    then
-      echo "-- Updating files in bower-angular"
-      cp $BUILD_DIR/angular.* $BOWER_ANGULAR_DIR
-  fi
+  for repo in "${REPOS[@]}"
+  do
+    if [ -f $BUILD_DIR/$repo.js ] # ignore i18l
+      then
+        echo "-- Updating files in bower-$repo"
+        cp $BUILD_DIR/$repo.* $TMP_DIR/bower-$repo/
+    fi
+  done
 
   # move i18n files
 #  cp $BUILD_DIR/i18n/*.js $TMP_DIR/bower-angular-i18n/
 
   # move csp.css
-  cp $BUILD_DIR/angular-csp.css $BOWER_ANGULAR_DIR
+  cp $BUILD_DIR/angular-csp.css $TMP_DIR/bower-angular
 
 
   #
   # Run local precommit script if there is one
   #
-  if [ -f $BOWER_ANGULAR_DIR/precommit.sh ]
-    then
-      echo "-- Running precommit.sh script for bower-angular"
-      cd $BOWER_ANGULAR_DIR
-      ./precommit.sh
-      cd $SCRIPT_DIR
-  fi
-
+  for repo in "${REPOS[@]}"
+  do
+    if [ -f $TMP_DIR/bower-$repo/precommit.sh ]
+      then
+        echo "-- Running precommit.sh script for bower-$repo"
+        cd $TMP_DIR/bower-$repo
+        $TMP_DIR/bower-$repo/precommit.sh
+        cd $SCRIPT_DIR
+    fi
+  done
 
   #
   # update bower.json
   # tag each repo
   #
-  echo "-- Updating version in bower-angular to $NEW_VERSION"
-  cd $BOWER_ANGULAR_DIR
-  replaceJsonProp "bower.json" "version" ".*" "$NEW_VERSION"
-  replaceJsonProp "bower.json" "angular.*" ".*" "$NEW_VERSION"
-  replaceJsonProp "package.json" "version" ".*" "$NEW_VERSION"
-  replaceJsonProp "package.json" "angular.*" ".*" "$NEW_VERSION"
+  for repo in "${REPOS[@]}"
+  do
+    echo "-- Updating version in bower-$repo to $NEW_VERSION"
+    cd $TMP_DIR/bower-$repo
+    replaceJsonProp "bower.json" "version" ".*" "$NEW_VERSION"
+    replaceJsonProp "bower.json" "angular.*" ".*" "$NEW_VERSION"
+    replaceJsonProp "package.json" "version" ".*" "$NEW_VERSION"
+    replaceJsonProp "package.json" "angular.*" ".*" "$NEW_VERSION"
 
-  git add -A
+    git add -A
 
-  echo "-- Committing and tagging bower-angular"
-  git commit -m "v$NEW_VERSION"
-  git tag v$NEW_VERSION
-  cd $SCRIPT_DIR
+    echo "-- Committing and tagging bower-$repo"
+    git commit -m "v$NEW_VERSION"
+    git tag v$NEW_VERSION
+    cd $SCRIPT_DIR
+  done
 }
 
 function publish {
-  echo "-- Pushing bower-angular"
-  cd $BOWER_ANGULAR_DIR
-  git push origin master
-  git push origin v$NEW_VERSION
+  for repo in "${REPOS[@]}"
+  do
+    echo "-- Pushing bower-angular"
+    cd $TMP_DIR/bower-$repo
+    git push origin master
+    git push origin v$NEW_VERSION
 
-  # don't publish every build to npm
-#  if [ "${NEW_VERSION/+sha}" = "$NEW_VERSION" ] ; then
-#    echo "-- Publishing to npm as $DIST_TAG"
-#    npm publish --tag=$DIST_TAG
-#  fi
+    # don't publish every build to npm
+#    if [ "${NEW_VERSION/+sha}" = "$NEW_VERSION" ] ; then
+#      echo "-- Publishing to npm as $DIST_TAG"
+#      npm publish --tag=$DIST_TAG
+#    fi
 
-  cd $SCRIPT_DIR
+    cd $SCRIPT_DIR
+  done
 }
 
 source $(dirname $0)/../utils.inc
